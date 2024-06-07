6358b89c754aa0bda6e719fa9b8a9335cbdf55eb
Antonio Lukic <alukic@x3000c0s27b0n0.hsn.hpc.srce.hr>
2024-06-07 09:36:05 +0200

    added upsampled things

--- train_reply_BERT.py
+++ train_reply_BERT.py
@@ -8,15 +8,17 @@ from torch.utils.data import TensorDataset, DataLoader, RandomSampler, Sequentia
 from transformers import BertForSequenceClassification
 from transformers import get_linear_schedule_with_warmup
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from sklearn.metrics import classification_report
+from sklearn.utils import resample
 
 
 def merge_datasets(dataset_name):
     df_train_reply = pd.read_csv(f'processed_dataset/reply_{dataset_name}.csv')
     df_valid_reply = pd.read_csv('processed_dataset/reply_valid.csv')
     df_test_reply = pd.read_csv(f'processed_dataset/test_reply_{dataset_name}.csv')
-    df_train_pred = pd.read_csv('train_pred.txt', sep='\t')
+    df_train_pred = pd.read_csv(f'train_pred_{dataset_name}.txt', sep='\t')
     df_valid_pred = pd.read_csv('valid_pred.txt', sep='\t')
-    df_test_pred = pd.read_csv('test_pred.txt', sep='\t')
+    df_test_pred = pd.read_csv(f'test_pred_{dataset_name}.txt', sep='\t')
 
     df_train_reply['SourceKey'] = df_train_reply['SourceKey'].astype(str)
     df_valid_reply['SourceKey'] = df_valid_reply['SourceKey'].astype(str)
@@ -31,13 +33,13 @@ def merge_datasets(dataset_name):
     df_train['combined_text'] = df_train['Text_x'] + " [LABEL] " + df_train['Prediction'].astype(str) + " [SEP] " + df_train['Text_y']
     df_valid['combined_text'] = df_valid['Text_x'] + " [LABEL] " + df_valid['Prediction'].astype(str) + " [SEP] " + df_valid['Text_y']
     df_test['combined_text'] = df_test['Text_x'] + " [LABEL] " + df_test['Prediction'].astype(str) + " [SEP] " + df_test['Text_y']
-    
+
     return df_train, df_valid, df_test
 
 def tokenize(df_train, df_valid, df_test):  # change no context or context the column: Text_x, combined_text
-    tokenized_train = df_train['Text_x'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, max_length=512, truncation=True))
-    tokenized_valid = df_valid['Text_x'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, max_length=512, truncation=True))
-    tokenized_test = df_test['Text_x'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, max_length=512, truncation=True))
+    tokenized_train = df_train['combined_text'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, max_length=512, truncation=True))
+    tokenized_valid = df_valid['combined_text'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, max_length=512, truncation=True))
+    tokenized_test = df_test['combined_text'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, max_length=512, truncation=True))
     max_len = max([len(sen) for sen in pd.concat([tokenized_train, tokenized_valid, tokenized_test], axis = 0)])
     return tokenized_train, tokenized_valid, tokenized_test, max_len
 
@@ -59,6 +61,22 @@ def create_iml(df, tokenized, max_len):
 
     return inputs, masks, labels
 
+
+def create_data_samples(df_train, df_valid, df_test):
+    train_tokenized, valid_tokenized, test_tokenized, max_len = tokenize(df_train, df_valid, df_test)
+    train_iml = create_iml(df_train, train_tokenized, max_len)
+    valid_iml = create_iml(df_valid, valid_tokenized, max_len)
+    test_iml = create_iml(df_test, test_tokenized, max_len)
+
+    train_data = TensorDataset(*train_iml)
+    train_sampler = RandomSampler(train_data)
+    validation_data = TensorDataset(*valid_iml)
+    validation_sampler = SequentialSampler(validation_data)
+    test_data = TensorDataset(*test_iml)
+    test_sampler = SequentialSampler(test_data)
+
+    return train_data, train_sampler, validation_data, validation_sampler, test_data, test_sampler
+
 def flat_accuracy(preds, labels, show=False):
     pred_flat = np.argmax(preds, axis=1).flatten()
     labels_flat = labels.flatten()
@@ -76,30 +94,40 @@ def set_seeds(seed):
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-set_seeds(42)
+seed = 42
+set_seeds(seed)
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-dataset_name = 'twitter'
+dataset_name = 'redit'
+dataset_name_2 = 'twitter'
 df_train, df_valid, df_test = merge_datasets(dataset_name)
+#df_train_2, _, df_test_2 = merge_datasets(dataset_name_2)
+if dataset_name_2 is not None:
+    df_train_2, _, df_test_2 = merge_datasets(dataset_name_2)
+    df_train = pd.concat([df_train, df_train_2], ignore_index=True)
+    df_test = pd.concat([df_test, df_test_2], ignore_index=True)
 
-train_tokenized, valid_tokenized, test_tokenized, max_len = tokenize(df_train, df_valid, df_test)
-train_iml = create_iml(df_train, train_tokenized, max_len)
-valid_iml = create_iml(df_valid, valid_tokenized, max_len)
-test_iml = create_iml(df_test, test_tokenized, max_len)
+def upsample(df):
+    df_majority = df[df.Label_x == 3]  # comments
+    df_minority1 = df[df.Label_x == 0]
+    df_minority2 = df[df.Label_x == 1]
+    df_minority3 = df[df.Label_x == 2]
 
-train_data = TensorDataset(*train_iml)
-train_sampler = RandomSampler(train_data)
-validation_data = TensorDataset(*valid_iml)
-validation_sampler = SequentialSampler(validation_data)
-test_data = TensorDataset(*test_iml)
-test_sampler = SequentialSampler(test_data)
+    df_minority1_upsampled = resample(df_minority1, replace=True, n_samples=len(df_majority), random_state=seed)
+    df_minority2_upsampled = resample(df_minority2, replace=True, n_samples=len(df_majority), random_state=seed)
+    df_minority3_upsampled = resample(df_minority3, replace=True, n_samples=len(df_majority), random_state=seed)
+
+    return pd.concat([df_majority, df_minority1_upsampled, df_minority2_upsampled, df_minority3_upsampled])
+
+train_data, train_sampler, validation_data, validation_sampler, test_data, test_sampler = create_data_samples(upsample(df_train), df_valid, upsample(df_test))
+train_data_2, train_sampler_2, validation_data_2, validation_sampler_2, test_data_2, test_sampler_2 = create_data_samples(df_train, df_valid, df_test)
 
 best_accuracy = 0.0
 best_model = None
 best_hyperparameters = {}
-
-batch_sizes = [16, 32]
-learning_rate = [2e-5, 2e-4, 2e-5]
-num_epochs = [2, 3, 4]
+model = None
+batch_sizes = [32]
+learning_rate = [2e-5]
+num_epochs = [5]
 
 for batch_size in batch_sizes:
     for lr in learning_rate:
@@ -211,14 +239,14 @@ for batch_size in batch_sizes:
 
 print(best_hyperparameters)
 
-best_model_instance = BertForSequenceClassification.from_pretrained(
-    "bert-base-uncased",
-    num_labels=4,
-    output_attentions=False,
-    output_hidden_states=False,
-)
-best_model_instance.load_state_dict(best_model)
-best_model_instance.to(device)
+# best_model_instance = BertForSequenceClassification.from_pretrained(
+#     "bert-base-uncased",
+#     num_labels=4,
+#     output_attentions=False,
+#     output_hidden_states=False,
+# )
+# best_model_instance.load_state_dict(best_model)
+# best_model_instance.to(device)
 
 # predicting on data
 def make_predictions(data_loader):
@@ -228,16 +256,33 @@ def make_predictions(data_loader):
         b_input_ids, b_input_mask, b_labels = batch
 
         with torch.no_grad():
-            outputs = best_model_instance(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+            outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
         logits = outputs[0]
         preds = torch.argmax(logits, dim=1)
         predictions.extend(preds.detach().cpu().numpy())
     return predictions
 
-df_train['Prediction_x'] = make_predictions(train_dataloader)
-df_valid['Prediction_x'] = make_predictions(validation_dataloader)
-df_test['Prediction_x'] = make_predictions(test_dataloader)
+train_dataloader_2 = DataLoader(
+    train_data_2,
+    sampler = train_sampler_2,
+    batch_size = batch_size
+)
+validation_dataloader_2 = DataLoader(
+    validation_data_2,
+    sampler = validation_sampler_2,
+    batch_size = batch_size
+)
+test_dataloader_2 = DataLoader(
+    test_data_2,
+    sampler = test_sampler_2,
+    batch_size = batch_size
+)
 
-print("Train Accuracy: ", accuracy_score(df_train['Label_x'], df_train['Prediction_x']))
-print("Validation Accuracy: ", accuracy_score(df_valid['Label_x'], df_valid['Prediction_x']))
-print("Test Accuracy: ", accuracy_score(df_test['Label_x'], df_test['Prediction_x']))
+df_train['Prediction_x'] = make_predictions(train_dataloader_2)
+df_valid['Prediction_x'] = make_predictions(validation_dataloader_2)
+df_test['Prediction_x'] = make_predictions(test_dataloader_2)
+
+
+print("Train report:\n", classification_report(df_train['Label_x'], df_train['Prediction_x']))
+print("Validation report:\n", classification_report(df_valid['Label_x'], df_valid['Prediction_x']))
+print("Test report:\n", classification_report(df_test['Label_x'], df_test['Prediction_x']))
